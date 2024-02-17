a1d6c7a28b31a12aca2a86931c7e08305ef78540
Abilashakuruva <kuruvaabilasha218@gmail.com>
2024-02-17 22:17:48 +0530

    init

---
+++ client/src/pages/chat/index.js
@@ -0,0 +1,19 @@
+import styles from './styles.module.css';
+import RoomAndUsersColumn from './room-and-users';
+import MessagesReceived from './messages';
+import SendMessage from './send-message';
+
+
+const Chat = ({ username, room, socket }) => {
+  return (
+    <div className={styles.chatContainer}>
+      <div>
+        <MessagesReceived socket={socket} />
+        <SendMessage socket={socket} username={username} room={room} />
+
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
